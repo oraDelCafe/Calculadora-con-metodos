78aaa8eddbb00a03c86aa1550c2cc1ff80f4f00f
oraDelCafe <pedro_ortega1993@live.com>
2025-10-23 23:27:29 +0200

    ultimos retoques de calculadora

--- src/main/java/daw/Calculadora.java
+++ src/main/java/daw/Calculadora.java
@@ -1,56 +1,96 @@
 package daw;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class Calculadora {
 
     public static void main(String[] args) {
         /**
          * Hago la calculadora con metodos
-         * bububu
          */
 
         boolean salir = false;
         Scanner sc = new Scanner(System.in);
         int opcionElegida, operando1 = 0, operando2 = 0;
 
-        String textoMenu = """
-                Calculadora:
-                1-.Sumar
-                2-.Restar
-                3-.Multiplicar
-                4-.Dividir
-                5-.Salir
-                """;
+        final String TEXTO_MENU = """
+                \n
+                    Calculadora:
+                    1-.Sumar
+                    2-.Restar
+                    3-.Multiplicar
+                    4-.Dividir
+                    5-.Salir
+                    """;
 
         do {
+
             opcionElegida = 0;
-            // cambiar a metodo de menu
-
-            System.out.println(textoMenu);
-
+            // renuevo vatiable.
             try {
-                opcionElegida = sc.nextInt();
+                // do {
+                // controlo opciones
+                opcionElegida = Integer.parseInt(MetodosCalculadora.Menu(TEXTO_MENU));
+                // } while (opcionElegida > 0 && opcionElegida < 6);
+                // 1 2 3 4 5 no hace falta ya que controlo por default
 
                 switch (opcionElegida) {
                     case 1 -> {
+                        // suma
+                        // pido numeros
+                        operando1 = MetodosCalculadora.PreguntarNumero();
+                        operando2 = MetodosCalculadora.PreguntarNumero();
+                        JOptionPane.showMessageDialog(null,
+                                "La suma de es = " + MetodosCalculadora.Sumar(operando1, operando2));
+
                     }
                     case 2 -> {
+                        // resta
+                        // pido numeros
+                        operando1 = MetodosCalculadora.PreguntarNumero();
+                        operando2 = MetodosCalculadora.PreguntarNumero();
+
+                        JOptionPane.showMessageDialog(null,
+                                "La resta de es = " + MetodosCalculadora.Restar(operando1, operando2));
+
                     }
                     case 3 -> {
+                        // multiplico
+                        // pido numeros
+                        operando1 = MetodosCalculadora.PreguntarNumero();
+                        operando2 = MetodosCalculadora.PreguntarNumero();
+                        JOptionPane.showMessageDialog(null,
+                                "La multiplicacion de es = " + MetodosCalculadora.Multiplicar(operando1, operando2));
+
                     }
                     case 4 -> {
+                        // Divido
+                        // pido numeros
+                        operando1 = MetodosCalculadora.PreguntarNumero();
+                        operando2 = MetodosCalculadora.PreguntarNumero();
+                        try {
+                            JOptionPane.showMessageDialog(null,
+                                    "La division de es = " + MetodosCalculadora.Dividir(operando1, operando2));
+                        } catch (ArithmeticException ae) {
+                            JOptionPane.showMessageDialog(null, "No se puede dividir por 0 ");
+                        }
+
                     }
                     case 5 -> {
+                        // sale
+                        JOptionPane.showMessageDialog(null, "Adios");
+                        salir = true;
                     }
                     default -> {
-                        System.out.println("Numero fuera de rango");
+                        JOptionPane.showMessageDialog(null, "Numero fuera de rango");
                     }
                 }
 
-            } catch (InputMismatchException ime) {
-                System.out.println("Has metido una letra");
+            } catch (NumberFormatException nfe) {
+                JOptionPane.showMessageDialog(null, "Has introducido una letra");
+
             }
 
         } while (!salir);
--- src/main/java/daw/MetodosCalculadora.java
+++ src/main/java/daw/MetodosCalculadora.java
@@ -1,6 +1,49 @@
 package daw;
 
+import javax.swing.JOptionPane;
+
 public class MetodosCalculadora {
     // Aqui voy ha hacer los metodos
+    // voy a probar
+    public static String Menu(String texto) {
+        // imprime texto y recoge datos
+        String respuesta = "";
 
+        respuesta = JOptionPane.showInputDialog(texto);
+
+        return respuesta;
+    }
+
+    public static int PreguntarNumero() {
+        // pide numero y devuelve
+        int respuestaInt = 0;
+        do {
+            respuestaInt = Integer.parseInt(JOptionPane.showInputDialog("Intoduce numero"));
+            if (respuestaInt >= 0) {
+                JOptionPane.showInputDialog("Intoduce numero Mayor o igual que 0");
+            }
+        } while (!(respuestaInt >= 0));
+        return respuestaInt;
+
+    }
+
+    public static int Sumar(int a, int b) {
+        // suma
+        return a + b;
+    }
+
+    public static int Restar(int a, int b) {
+        // Resta
+        return a - b;
+    }
+
+    public static int Multiplicar(int a, int b) {
+        // Multiplica
+        return a * b;
+    }
+
+    public static double Dividir(int a, int b) {
+        // Divide
+        return a / b;
+    }
 }
